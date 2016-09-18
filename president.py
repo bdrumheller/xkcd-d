c90d959cd90db53aac42d383adbfcf23429d6d16
SamuelWenninger <wennis@rpi.edu>
2016-09-18 00:37:20 -0400

    Added image links to the json data

--- president.py
+++ president.py
@@ -6,5 +6,6 @@ presidents = json.load(stuff)
 for index, data in enumerate(presidents):
     print index
     presidents[index]["summary"] = wikipedia.summary(data["president"])
+    presidents[index]["img"] = wikipedia.page(data["president"]).images[0]
 with open('complete-presidents.json', 'w') as f:
     f.write(json.dumps(presidents))
