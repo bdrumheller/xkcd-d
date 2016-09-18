8c7172b063fda3ee61b64eb2c9cb0dfdf85fd028
SamuelWenninger <wennis@rpi.edu>
2016-09-17 22:13:19 -0400

    Works on Bing

--- content.js
+++ content.js
@@ -1,13 +1,24 @@
-function inject() {
+function inject(browser) {
     function sync(datums) {
+        console.log(browser)
         var url;
         if (datums.length) {
             url = datums[0].img;
             alt = datums[0].alt;
             var content = "<div id='collapse' class='collapse in'>"+
                 "<img id='xkcd' alt='"+alt+"' title='"+alt+"' src='"+url+"' data-toggle='collapse' data-target='#collapse'/></div>";
-            jQuery(".appbar").after(content);
+            if (browser == "google") {
+                jQuery(".appbar").after(content);
+            }
+            else if (browser == "bing") {
+                jQuery("#b_tween").after(content);
+            }
         }
     }
-    hound.search($("#lst-ib").val(), sync)
+    if (browser == "google") {
+        hound.search($("#lst-ib").val(), sync)
+    }
+    else if (browser == "bing") {
+        hound.search($(".b_searchbox").val(), sync)
+    }
 }
--- xkcd-injector.js
+++ xkcd-injector.js
@@ -6,11 +6,21 @@ var dataSet;
 var hound;
 
 var lastvalue
-function oktoinject(){
-    if ($("#lst-ib").val() !== lastvalue){
-        lastvalue = $("#lst-ib").val()
-        jQuery('#collapse').remove()
-        inject()
+
+function oktoinject(browser){
+    if (browser == "google") {
+        if ($("#lst-ib").val() !== lastvalue){
+            lastvalue = $("#lst-ib").val()
+            jQuery('#collapse').remove()
+            inject(browser)
+        }
+    }
+    if (browser == "bing") {
+        if ($(".b_searchbox").val() !== lastvalue){
+            lastvalue = $(".b_searchbox").val()
+            jQuery('#collapse').remove()
+            inject(browser)
+        }
     }
 }
 
@@ -22,6 +32,13 @@ function initialize(){
         queryTokenizer: Bloodhound.tokenizers.whitespace,
         datumTokenizer: dataPoint => Bloodhound.tokenizers.whitespace(dataPoint.alt)
     });
-    setInterval(oktoinject, 300)
+    var bingPattern = /bing/i;
+    var googlePattern = /google/i;
+    if (googlePattern.test(window.location.href)) {
+        setInterval(oktoinject("google"), 300)
+    }
+    else if (bingPattern.test(window.location.href)) {
+        setInterval(oktoinject("bing"), 300)
+    }
 }
 jQuery(initialize);
