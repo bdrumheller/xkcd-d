d4a1ecf7a8412ea8d354a7c39168eb6b756b834b
John Drogo <droatsyr@gmail.com>
2016-09-17 21:28:50 -0400

    new style

--- content.js
+++ content.js
@@ -2,10 +2,14 @@ function inject() {
     function sync(datums) {
         var url;
         if (datums.length) {
-            url = datums[0].img;
-            alt = datums[0].alt;
-            var content = "<div id='collapse' class='collapse in'>"+
-                "<img id='xkcd' alt='"+alt+"' title='"+alt+"' src='"+url+"' data-toggle='collapse' data-target='#collapse'/></div>";
+            var content = `<div id="collapse" class="collapse in">
+                <img id="xkcd" alt="${datums[0].alt}" title="${datums[0].alt}" src="${datums[0].img}" data-toggle="collapse" data-target="#collapse"/>
+                <section>
+                    <h3>${datums[0].title}</h3>
+                    <span id="date">Published: ${datums[0].month}/${datums[0].day}/${datums[0].year}</span>
+                    <p>${datums[0].alt}</p>
+                </section>
+            </div>`
             jQuery(".appbar").after(content);
         }
     }
